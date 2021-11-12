a7065700e4e0c9ebc06a170e4ed1a1338f8985fd
del <del.xiong@harman.com>
2021-11-12 22:23:56 +0800

    改为从启动参数加载配置信息

--- config/param.go
+++ config/param.go
@@ -7,7 +7,7 @@ var (
 	SMTP_EMAIL   = "test2@alimail.alidddd.com" // smtp账号
 	SMTP_PWD     = "aaaaaaaaaaaaaaa"           // smtp密码
 	SMTP_RECEIPT = "me@qq.com"                 // 接收通知人
-	MysqlConnect = "datereminder:password@tcp(localhost)/datereminder?charset=utf8mb4"
+	MysqlConnect = "datereminder:password@tcp(localhost)/datereminder"
 )
 
 var Engine *xorm.Engine
